5e5aa3e1a8e184f0d523d48c2c78c0d2851359ad
Freedom Loisa <kitakayaloisa@gmail.com>
2022-10-16 21:02:36 +0300

    cleaned code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,53 +1,13 @@
-import { useEffect } from "react";
-import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-
 import axios from "axios";
 
 const url = "http://localhost:1337/api/products/";
 
-//  let ourData = [];
-
-const createData = () => {
-  axios
-  .post(url, {
-    description: "This is a cable",
-    price: "15000",
-  })
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.log(error.response.data);
-  });
-};
 const Home = () => {
-  // const [products] = useState([0]);
-
-  useEffect(() => {
-    axios.get(url).then((response) => {
-      // setProducts(response.data); 
-
-      // ourData = (response.data);
-
-      console.log(response.data);
-    });
-  }, []);
-
   return (
     <div className="container">
-        <h1>Home</h1>
-              
-        <Link to="/">Home</Link>
-        <Link to="/allproducts">AllProducts</Link>
-        <Link to="/singleproduct">SingleProduct</Link>
-        <Link to="/checkout">CheckOut</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Button variant="primary">Primary</Button>{' '}
-        <button className="btn btn-sm btn-success" onClick={createData}>Create Data</button>
+      <h1>Home</h1>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
