49ef3261ca432a98a489ff41a42c4aa89c033b8e
Freedom Loisa <kitakayaloisa@gmail.com>
2022-10-18 21:10:31 +0300

    file structure agreement

---
+++ src/pages/site/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div>
+      {/* navbar */}
+      {/* navbar */}
+
+      {/* current page */}
+      {/* current page */}
+
+      {/* footer */}
+      {/* footer */}
+    </div>
+  );
+};
+
+export default Home;
