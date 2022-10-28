f081685d99db4b6445103607425d7370bbd820bb
Freedom Loisa <kitakayaloisa@gmail.com>
2022-10-28 06:59:20 +0300

    created basic homepage

---
+++ src/components/Home/AboutSummary.jsx
@@ -0,0 +1,18 @@
+const AboutSummary = () => {
+  return (
+    <div className="company py-5">
+      <div className="w-50 mx-auto">
+        <h3 className="fs-3 text-center mb-3">Welcome to company</h3>
+        <p className="text-center">
+          Since 1979, we have been one of the most pioneering grocers in the
+          country. We lead by example; our trusted suppliers grow and produce
+          according to sustainable, organic principles because we all need to
+          respect and care for the land we work and the animals that call it
+          home.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSummary;
